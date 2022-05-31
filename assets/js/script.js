eaabad236efbc7c7f1f75d550c7bd9731013acc1
justin <fezzygo@gmail.com>
2022-05-31 00:37:02 -0400

    add days left functionality (js:12-27) to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-var clock = $("#clock")
+var clock = $("#clock");
+var datePicker = $("#datepicker");
+var daysLeft = $("#days-left");
 $( "#datepicker" ).datepicker();
 $( "#project-type" ).selectmenu();
 
@@ -6,4 +8,20 @@ setInterval(function() {
     clock.text(moment().format("hh:mm:ss a"));
 }, 1000);
 
+// Eventlistners
+datePicker.on("change", getDaysLeft )
 
+// calc the number of days left till due day
+function getDaysLeft(){
+    var formatedDate = changeDateFormat(datePicker[0].value);
+    console.log(formatedDate);
+    var futureDate = moment(formatedDate);
+    var todaysDate = moment();
+    daysLeft.text(`Days until due: ${futureDate.diff(todaysDate, 'days') +2 }`); 
+}
+
+// Change Date format (year-month-day)
+function changeDateFormat(date){
+    var splitDate = date.split("/");
+    return `${splitDate[2]}-${splitDate[0]}-${splitDate[1]}`
+}
