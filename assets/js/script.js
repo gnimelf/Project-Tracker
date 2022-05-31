c761b5948f9df253469bd2dc1bbb478a3f14bd0d
justin <fezzygo@gmail.com>
2022-05-31 08:50:00 -0400

    added function to calculate the est. total earned (js:33-37)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var clock = $("#clock");
 var datePicker = $("#datepicker");
 var daysLeft = $("#days-left");
-$( "#datepicker" ).datepicker();
-$( "#project-type" ).selectmenu();
+var hourlyWage = $("#hourly-wage")
+var estTotalEarned = $("#est-total-earned");
+$("#datepicker").datepicker();
+$("#project-type").selectmenu();
 
+// Clock
 setInterval(function() {
     clock.text(moment().format("hh:mm:ss a"));
 }, 1000);
@@ -11,13 +14,14 @@ setInterval(function() {
 // Eventlistners
 datePicker.on("change", getDaysLeft )
 
-// calc the number of days left till due day
-function getDaysLeft(){
-    var formatedDate = changeDateFormat(datePicker[0].value);
-    console.log(formatedDate);
+// Calculate the number of days left till due day
+function getDaysLeft(event){
+    var formatedDate = changeDateFormat(event.currentTarget.value);
     var futureDate = moment(formatedDate);
     var todaysDate = moment();
-    daysLeft.text(`Days until due: ${futureDate.diff(todaysDate, 'days') +2 }`); 
+    daysLeft.text(futureDate.diff(todaysDate, 'days') + 1); 
+    calcEstTotalEarned();
+    return;
 }
 
 // Change Date format (year-month-day)
@@ -25,3 +29,10 @@ function changeDateFormat(date){
     var splitDate = date.split("/");
     return `${splitDate[2]}-${splitDate[0]}-${splitDate[1]}`
 }
+
+// Calculate the Est. Total Earned
+function calcEstTotalEarned (){
+   console.log(estTotalEarned[0].innerHTML = daysLeft[0].innerHTML * (hourlyWage[0].value * 8));
+    return
+}
+
